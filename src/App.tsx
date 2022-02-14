cbdb9bbbe7d4c0e7c15278e68d7fe2e2ef791824
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-14 18:40:00 -0300

    Modelo 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+
+type Repository = {
+  full_name: string;
+  description: string;
+}
+
 function App() {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/renanvzd/repos')
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data);
+      })
+  }, [])
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      {repositories.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
