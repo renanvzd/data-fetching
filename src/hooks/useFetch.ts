5a2068fca684ae1ca41a264d32775ce87f36c24b
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-15 13:10:35 -0300

    Modelo 03 - Hook: useFetch - Error

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,11 +4,15 @@ import { useEffect, useState } from "react";
 export function useFetch<T = unknown>(url: string) {
   const [data, setData] = useState<T | null>(null)
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState<Error | null>(null)
 
   useEffect(() => {
     axios.get(url)
       .then(response => {
         setData(response.data);
+      })
+      .catch(err => {
+        setError(err);
       })
       .finally(() => {
         setIsFetching(false);
