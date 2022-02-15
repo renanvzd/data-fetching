a67f060c58ec83e80d8dc42dd052654c27a9dc03
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-15 15:03:35 -0300

    Modelo 04 - React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useFetch } from "./hooks/useFetch"
+import axios from 'axios';
+import { useQuery } from 'react-query'
 
 type Repository = {
   full_name: string;
@@ -6,14 +7,16 @@ type Repository = {
 }
 
 function App() {
+  const { data, isFetching } = useQuery<Repository[]>('repos', async () => {
+    const response = await axios.get('https://api.github.com/users/renanvzd/repos');
 
-  const { data: repositories, isFetching } =
-    useFetch<Repository[]>('users/renanvzd/repos')
+    return response.data;
+  })
 
   return (
     <ul>
       {isFetching && <p>Carregando...</p>}
-      {repositories?.map(repo => {
+      {data?.map(repo => {
         return (
           <li key={repo.full_name}>
             <strong>{repo.full_name}</strong>
